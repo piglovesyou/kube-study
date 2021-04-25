f8d3c53ed0600eca94444ce0d8ebd5fc8ee04baa
Soichi Takamura <thepiglovesyou@gmail.com>
2021-04-25 15:24:27 +0900

    It works!! on localhost:80

--- my-next-app/lib/redis.js
+++ my-next-app/lib/redis.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 import { promisify } from "util";
 
 const client = createClient({
-  host: "my-redis",
+  host: process.env.REDIS_HOST,
   password: "a",
 });
 
